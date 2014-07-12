f620d8b40e91804071ac8e0722c6126b1067ba60
Valeriy Bezuglyy <valnocorner@gmail.com>
2014-07-12 22:11:08 +0400

    added project

---
+++ VBMathMatrixExamples/VBAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  VBAppDelegate.h
+//  VBMathMatrixExamples
+//
+//  Created by Valeriy Bezuglyy on 12/07/14.
+//  Copyright (c) 2014 Valeriy Bezuglyy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface VBAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ VBMathMatrixExamplesTests/VBMathMatrixExamplesTests.m
@@ -0,0 +1,34 @@
+//
+//  VBMathMatrixExamplesTests.m
+//  VBMathMatrixExamplesTests
+//
+//  Created by Valeriy Bezuglyy on 12/07/14.
+//  Copyright (c) 2014 Valeriy Bezuglyy. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface VBMathMatrixExamplesTests : XCTestCase
+
+@end
+
+@implementation VBMathMatrixExamplesTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
